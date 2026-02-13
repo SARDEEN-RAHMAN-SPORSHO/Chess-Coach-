aa7300f1fd0ed0f4b6b7d2b561514858aa65b43e
Sporsho <94753278+SARDEEN-RAHMAN-SPORSHO@users.noreply.github.com>
2026-02-13 20:25:54 +0600

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  worker: {
+    format: 'es',
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'firebase': ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          'chess': ['chess.js', 'react-chessboard'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
